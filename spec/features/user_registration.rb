d42e3b957f352bd5a93573bee08ffe40e40e925e
Matt Payne <Payne@MattPayne.org>
2016-06-22 22:17:04 -0500

    bin/rspec spec/features/user_registration.rb

--- spec/features/user_registration.rb
+++ spec/features/user_registration.rb
@@ -1,12 +1,14 @@
-  describe 'UserRegistration' do
-    it 'allows a user to register' do
-      visit new_user_registration_path
-      #fill_in 'First name', :with => 'New'
-      #fill_in 'Last name', :with => 'User'
-      fill_in 'Email', :with => 'newuser@example.com'
-      fill_in 'Password', :with => 'userpassword'
-      fill_in 'Password Confirmation', :with => 'userpassword'
-      click_button 'Register'
-      page.should have content 'Welcome'
-    end
+require 'rails_helper'
+
+describe 'UserRegistration' do
+  it 'allows a user to register' do
+    visit new_user_registration_path
+    #fill_in 'First name', :with => 'New'
+    #fill_in 'Last name', :with => 'User'
+    fill_in 'Email', :with => 'newuser@example.com'
+    fill_in 'user_password', :with => 'userpassword'
+    fill_in 'user_password_confirmation', :with => 'userpassword'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome')
   end
+end
