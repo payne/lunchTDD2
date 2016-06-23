06ed06f1163c23d644d4d3092280e5fadd70679e
Matt Payne <Payne@MattPayne.org>
2016-06-22 22:22:28 -0500

    bin/rspec spec/features/user_registration.rb works but bin/rspec spec/features/create_group_spec.rb does not

--- spec/support/new_group_form.rb
+++ spec/support/new_group_form.rb
@@ -9,11 +9,14 @@ class NewGroupForm
   end
 
   def register
-    visit('/users/sign_up')
-    fill_in('Email', with: 'staff@tco.com')
-    fill_in('Password', with: 'Test!1234')
-    fill_in('Password confirmation', with: 'Test!1234')
-    click_on('Sign up')
+    visit new_user_registration_path
+    #fill_in 'First name', :with => 'New'
+    #fill_in 'Last name', :with => 'User'
+    fill_in 'Email', :with => 'newuser@example.com'
+    fill_in 'user_password', :with => 'userpassword'
+    fill_in 'user_password_confirmation', :with => 'userpassword'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome')
     self
   end
 
