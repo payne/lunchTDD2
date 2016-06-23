8160e04637c22c150a14133e339b97fb08e482f5
Matt Payne <Payne@MattPayne.org>
2016-06-22 21:39:07 -0500

    bin/rspec spec/models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
   resources :groups, only: [ :new, :create, :show, :index ]
+  root 'welcome#index'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires email' do
+      user = User.new(email: '')
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
 end
