97f67eb2a27d434932315edd89fae302de367f56
Matt Payne <Payne@MattPayne.org>
2016-06-22 22:40:04 -0500

    bin/rspec passes

--- spec/features/group_page_spec.rb
+++ spec/features/group_page_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 feature 'group page' do
   scenario 'group public page' do
+    visit new_user_registration_path
+    fill_in 'Email', :with => 'newuser@example.com'
+    fill_in 'user_password', :with => 'userpassword'
+    fill_in 'user_password_confirmation', :with => 'userpassword'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome')
+
     group_title = 'Java and JVM Junkies'
     group = FactoryGirl.create(:group, title: group_title) # in database (build is in RAM)
+    #print "group id is #{group.id}"
     visit("/groups/#{group.id}")
 
     expect(page).to have_content(group_title)
