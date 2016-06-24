08f5061e8f3953b7abb1240918a974d9911a6b9f
Matt Payne <Payne@MattPayne.org>
2016-06-23 21:13:57 -0500

    User registration now requires first and last name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lunch_events
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :groups, only: [ :new, :create, :show, :index ]
   root 'welcome#index'
   # From http://stackoverflow.com/questions/25858606/how-to-devise-destroy-session-and-sign-out-from-controller
