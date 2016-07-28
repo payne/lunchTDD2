d48e603a70e0cbabfad690341d35f8a30b4d2237
Matt Payne <Payne@MattPayne.org>
2016-07-28 06:23:50 -0500

    group is valid when users are valid

--- spec/models/group_assignment_spec.rb
+++ spec/models/group_assignment_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe GroupAssignment, type: :model do
   describe 'valid association demo' do
     it 'allows two users in a group' do
-      john = User.new(email: 'john@beatles.com')
-      paul = User.new(email: 'paul@beatles.com')
+      john = User.new(email: 'john@beatles.com', password: 'rock!1965')
+      paul = User.new(email: 'paul@beatles.com', password: 'rock!1965')
       beatles = Group.new(title: 'The Beatles')
       expect(beatles.valid?).to be(true)
-      #beatles.users << john
-      #beatles.users << paul
+      beatles.users << john
+      beatles.users << paul
       expect(beatles.valid?).to be(true)
     end
   end
