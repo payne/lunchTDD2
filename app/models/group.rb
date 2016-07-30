73dca72eaae606671737bd6c0cb6441089ca7a04
Matt Payne <Payne@MattPayne.org>
2016-07-29 22:41:30 -0500

    rails g lunch_event_assignments lunch_event:references group:references

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ActiveRecord::Base
   has_many :group_assignments
   has_many :users, through: :group_assignments
 
+  # Requires: rails g lunch_event_assignments lunch_event:references group:references
   has_many :lunch_event_assignments
   has_many :lunch_events, through: :lunch_event_assignments
 
