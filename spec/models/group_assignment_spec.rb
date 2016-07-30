7f4628eada2f8656527b065af807ac254aca9c76
Matt Payne <Payne@MattPayne.org>
2016-07-29 21:19:00 -0500

    fluid appends

--- spec/models/group_assignment_spec.rb
+++ spec/models/group_assignment_spec.rb
@@ -9,21 +9,18 @@ RSpec.describe GroupAssignment, type: :model do
       ringo = User.new(email: 'ringo@beatles.com', password: 'rock!1965')
       beatles = Group.new(title: 'The Beatles')
       expect(beatles.valid?).to be(true)
-      beatles.users << john
-      beatles.users << paul
-      beatles.users << george
-      beatles.users << ringo
+      beatles.users << john << paul << george << ringo
       expect(beatles.valid?).to be(true)
       expect(beatles.users.length()).to eq(4)
       linda = User.new(email: 'linda@wings.com', password: 'rock!1970')
       wings = Group.new(title: 'Wings')
-      wings.users << paul
-      wings.users << linda
+      wings.users << paul << linda
       expect(wings.users.length()).to eq(2)
       wings.save
       beatles.save
       paul.save
       expect(paul.groups.length()).to eq(2)
+      expect(ringo.groups.length()).to eq(1)
     end
   end
 end
