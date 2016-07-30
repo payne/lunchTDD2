1a67ba110e6b65b5a03f1ce4b2333df567f24eef
Matt Payne <Payne@MattPayne.org>
2016-07-29 21:15:27 -0500

    model_object.save important to get the relationships to flush.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def lunch_announcement(email)
     # This will soon take a group rather than an email.
-    mail to: email, subject: '54Lunch.com Announcement'
+    mail to: email, subject: '54Lunch.com Announcement', text: "blah, blah\n\n\nblah\blah!"
   end
 end
--- spec/models/group_assignment_spec.rb
+++ spec/models/group_assignment_spec.rb
@@ -5,11 +5,25 @@ RSpec.describe GroupAssignment, type: :model do
     it 'allows two users in a group' do
       john = User.new(email: 'john@beatles.com', password: 'rock!1965')
       paul = User.new(email: 'paul@beatles.com', password: 'rock!1965')
+      george = User.new(email: 'george@beatles.com', password: 'rock!1965')
+      ringo = User.new(email: 'ringo@beatles.com', password: 'rock!1965')
       beatles = Group.new(title: 'The Beatles')
       expect(beatles.valid?).to be(true)
       beatles.users << john
       beatles.users << paul
+      beatles.users << george
+      beatles.users << ringo
       expect(beatles.valid?).to be(true)
+      expect(beatles.users.length()).to eq(4)
+      linda = User.new(email: 'linda@wings.com', password: 'rock!1970')
+      wings = Group.new(title: 'Wings')
+      wings.users << paul
+      wings.users << linda
+      expect(wings.users.length()).to eq(2)
+      wings.save
+      beatles.save
+      paul.save
+      expect(paul.groups.length()).to eq(2)
     end
   end
 end
